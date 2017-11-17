ce866667a9b7b12177ffad33a03d07dd9a7e2faa
rmsoft1 <30922258+rmsoftware-development@users.noreply.github.com>
2017-11-17 18:33:17 -0500

    Versioning

--- RMSoftwareModularBot/Properties/AssemblyInfo.cs
+++ RMSoftwareModularBot/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.262.1")]
-[assembly: AssemblyFileVersion("1.3.262.1")]
+[assembly: AssemblyVersion("1.3.269.9")]
+[assembly: AssemblyFileVersion("1.3.269.9")]
