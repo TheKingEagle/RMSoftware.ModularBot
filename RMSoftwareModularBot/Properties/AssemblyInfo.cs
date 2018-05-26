109cc4fe3263b79f14888f954b57bf5ad0ea846b
rmsoft1 <30922258+rmsoftware-development@users.noreply.github.com>
2018-05-26 07:53:47 -0400

    Version Tick

--- RMSoftwareModularBot/Properties/AssemblyInfo.cs
+++ RMSoftwareModularBot/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.377.119")]
-[assembly: AssemblyFileVersion("1.3.377.119")]
+[assembly: AssemblyVersion("1.3.667.914")]
+[assembly: AssemblyFileVersion("1.3.667.914")]
