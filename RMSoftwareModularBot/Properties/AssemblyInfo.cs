c045a4a2be897931102c2c18e7ccfc32e290e0e7
TheKingEagle <30922258+rmsoftware-development@users.noreply.github.com>
2018-09-23 10:00:53 -0400

    Assembly Version Tick 1.4.44.1079

--- RMSoftwareModularBot/Properties/AssemblyInfo.cs
+++ RMSoftwareModularBot/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.14.1038")]
-[assembly: AssemblyFileVersion("1.4.14.1038")]
+[assembly: AssemblyVersion("1.4.55.1079")]
+[assembly: AssemblyFileVersion("1.4.55.1079")]
