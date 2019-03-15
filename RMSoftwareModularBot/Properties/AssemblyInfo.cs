2146c363eebf1510dd4b0d2e47b29ef978109828
TheKingEagle <30922258+rmsoftware-development@users.noreply.github.com>
2019-03-15 18:05:29 -0400

    V TICK (#1326)

--- RMSoftwareModularBot/Properties/AssemblyInfo.cs
+++ RMSoftwareModularBot/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.244.1268")]
-[assembly: AssemblyFileVersion("1.4.244.1268")]//File
+[assembly: AssemblyVersion("1.4.302.1326")]
+[assembly: AssemblyFileVersion("1.4.302.1326")]//File
