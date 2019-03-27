ebf839bc876fff4409b1fdcdabdb98480618a4cc
TheKingEagle <rmsoftware1@gmail.com>
2019-03-27 17:54:25 -0400

    Implemented console conmsg/setgch

--- ModularBOT/Component/ConsoleIO.cs
+++ ModularBOT/Component/ConsoleIO.cs
@@ -17,6 +17,9 @@ namespace ModularBOT.Component
         public bool Busy { get; private set; }
         public List<string> ARGS { get; private set; }
 
+        public int CurTop { get; private set; }
+        public int PrvTop { get; private set; }
+
         private string currentTitle = "";
         private ConsoleColor ConsoleForegroundColor = ConsoleColor.Gray;
         private ConsoleColor ConsoleBackgroundColor = ConsoleColor.Black;
@@ -35,8 +38,8 @@ namespace ModularBOT.Component
         /// <param name="title">Console's header title (not window title)</param>
         public void ConsoleGUIReset(ConsoleColor fore, ConsoleColor back, string title)
         {
-            
-            
+
+
             if (title.Length > 72)
             {
                 title = title.Remove(71) + "...";
@@ -100,15 +103,15 @@ namespace ModularBOT.Component
             string pTitle = WTitle.PadLeft(71 + WTitle.Length / 2);
             pTitle += "".PadRight(71 - WTitle.Length / 2);
             Console.Write("\u2551{0}\u2551", pTitle);
-            Console.Write("\u2551{0}\u2551","".PadLeft(142));
+            Console.Write("\u2551{0}\u2551", "".PadLeft(142));
             string progressBAR = "";
             float f = (float)(ProgressValue / (float)ProgressMax);
-            
-            int amt =  (int)(44 * (float)f);
+
+            int amt = (int)(44 * (float)f);
 
             for (int i = 0; i < 44; i++)
             {
-                if(i <= amt)
+                if (i <= amt)
                 {
 
                     progressBAR += "\u2588";
@@ -127,8 +130,8 @@ namespace ModularBOT.Component
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("\u2551");
             DecorateBottom();
-            
-           
+
+
             Console.BackgroundColor = back;
             Console.ForegroundColor = fore;
 
@@ -175,10 +178,10 @@ namespace ModularBOT.Component
             Console.ForegroundColor = fore;
             ConsoleBackgroundColor = back;
         }
-        
+
         //Heavily tweaked from: https://stackoverflow.com/questions/20534318/make-console-writeline-wrap-words-instead-of-letters
         //Fixed a bug where wrap would fail if no spaces & even if space, characters longer than console width would break)
-        public static string WordWrap(string paragraph, int consoleoffset = 23)
+        public string WordWrap(string paragraph, int consoleoffset = 23)
         {
             paragraph = new Regex(@" {2,}").Replace(paragraph.Trim(), @" ");
             //paragraph = new Regex(@"\r\n{2,}").Replace(paragraph.Trim(), @" ");
@@ -233,13 +236,13 @@ namespace ModularBOT.Component
         /// <param name="Entrycolor">An optional entry color. If none (or black), the message.LogSeverity is used for color instead.</param>
         public void WriteEntry(LogMessage message, ConsoleColor Entrycolor = ConsoleColor.Black, bool showCursor = true)
         {
-            
+
             if (Busy)
             {
                 SpinWait.SpinUntil(() => !Busy);//This will help prevent the console from being sent into a mess of garbled words.
             }
             Busy = true;
-
+            PrvTop = Console.CursorTop;
             Console.SetCursorPosition(0, Console.CursorTop);//Reset line position.
             LogMessage l = new LogMessage(message.Severity, message.Source.PadRight(11, '\u2000'), message.Message, message.Exception);
             string[] lines = WordWrap(l.ToString()).Split('\n');
@@ -318,7 +321,7 @@ namespace ModularBOT.Component
             if (showCursor)
             {
                 Console.Write(">");//Write the input indicator.
-                
+
             }
             //Program.CursorPTop = Console.CursorTop;//Set the cursor position, this will delete ALL displayed input from console when it is eventually reset.
             Thread.Sleep(1);//safe.
@@ -326,6 +329,7 @@ namespace ModularBOT.Component
             Console.ForegroundColor = ConsoleForegroundColor;
             Console.CursorVisible = showCursor;
             Busy = false;
+            CurTop = Console.CursorTop;
         }
 
         /// <summary>
@@ -340,11 +344,12 @@ namespace ModularBOT.Component
             {
                 SpinWait.SpinUntil(() => !Busy);//This will help prevent the console from being sent into a mess of garbled words.
             }
+            PrvTop = Console.CursorTop;
             Busy = true;
 
             Console.SetCursorPosition(0, Console.CursorTop);//Reset line position.
-           
-            string[] lines = WordWrap(message,1).Split('\n');
+
+            string[] lines = WordWrap(message, 1).Split('\n');
             ConsoleColor bglast = ConsoleBackgroundColor;
 
 
@@ -390,6 +395,7 @@ namespace ModularBOT.Component
             Console.ForegroundColor = ConsoleForegroundColor;
             Console.CursorVisible = showCursor;
             Busy = false;
+            CurTop = Console.CursorTop;
         }
 
         /// <summary>
@@ -436,7 +442,7 @@ namespace ModularBOT.Component
                 Console.Write("\u2550");
             }
         }
-        
+
         /// <summary>
         /// Show 'CRASH' screen with custom title and call for app termination or restart.
         /// </summary>
@@ -446,7 +452,7 @@ namespace ModularBOT.Component
         /// <param name="timeout">auto restart timeout in seconds.</param>
         /// <param name="ex">The inner exception leading to the killscreen.</param>
         /// <returns></returns>
-        public Task<bool> ShowKillScreen(string title, string message, bool autorestart,ref bool ShutdownCalled, int timeout = 5, Exception ex = null)
+        public Task<bool> ShowKillScreen(string title, string message, bool autorestart, ref bool ShutdownCalled, int timeout = 5, Exception ex = null)
         {
             ConsoleGUIReset(ConsoleColor.White, ConsoleColor.DarkRed, title);
             WriteEntry(new LogMessage(LogSeverity.Critical, "MAIN", "The program encountered a problem, and was terminated. Details below."));
@@ -525,7 +531,7 @@ namespace ModularBOT.Component
             }
         }
 
-        public void WriteErrorsLog(string message, Exception ex=null)
+        public void WriteErrorsLog(string message, Exception ex = null)
         {
             using (FileStream fs = new FileStream("ERRORS.LOG", FileMode.Append))
             {
@@ -547,45 +553,27 @@ namespace ModularBOT.Component
         /// <summary>
         /// Process console commands
         /// </summary>
-        public void GetConsoleInput(ref bool ShutdownCalled, ref bool RestartRequested)
+        public void GetConsoleInput(ref bool ShutdownCalled, ref bool RestartRequested, ref DiscordNET discordNET)
         {
             ulong chID = 0;
-            
+
             while (true)
             {
-                int cl = Console.CursorLeft;
-                int ct = Console.CursorTop;
                 string input = Console.ReadLine();
-                Console.SetCursorPosition(cl, ct);//reset
+                Console.CursorTop = CurTop;
                 WriteEntry(new LogMessage(LogSeverity.Info, "Console", input));
                 if (input.ToLower() == "stopbot")
                 {
                     WriteEntry(new LogMessage(LogSeverity.Critical, "MAIN", "Console session called STOPBOT."));
-                    cl = Console.CursorLeft;
-                    ct = Console.CursorTop;
 
-                    WriteEntry(new LogMessage(LogSeverity.Warning, "Session", "Ending session. and closing program in 3..."), ConsoleColor.Black, false);
-                    Console.SetCursorPosition(cl, ct);//reset
-                    //_client.SetGameAsync("");
-                    Thread.Sleep(1000);
-
-                    WriteEntry(new LogMessage(LogSeverity.Warning, "Session", "Ending session. and closing program in 2..."), ConsoleColor.Black, false);
-                    Console.SetCursorPosition(cl, ct);//reset
-                    //_client.SetStatusAsync(UserStatus.Invisible);
-                    Thread.Sleep(1000);
-                    WriteEntry(new LogMessage(LogSeverity.Warning, "Session", "Ending session. and closing program in 1..."), ConsoleColor.Black, false);
-                    Console.SetCursorPosition(cl, ct);//reset
-                    //_client.StopAsync();
-                    Thread.Sleep(1000);
-
-                    ShutdownCalled = true;
+                    discordNET.Stop(ref ShutdownCalled);
                     RestartRequested = false;
                     break;
                 }
                 if (input.ToLower() == "rskill")
                 {
 
-                    RestartRequested = ShowKillScreen("Test KS", "The program was instructed to run a test killscreen. This will auto restart the program.", true,ref ShutdownCalled, 5, new ApplicationException("Command rskill triggered kill screen. USER INITIATED CRASH SCREEN.")).GetAwaiter().GetResult();
+                    RestartRequested = ShowKillScreen("Test KS", "The program was instructed to run a test killscreen. This will auto restart the program.", true, ref ShutdownCalled, 5, new ApplicationException("Command rskill triggered kill screen. USER INITIATED CRASH SCREEN.")).GetAwaiter().GetResult();
                     break;
                 }
                 if (input.ToLower() == "cls" || input.ToLower() == "clear")
@@ -618,7 +606,7 @@ namespace ModularBOT.Component
                 {
                     string status = input.Remove(0, 7).Trim();
                     WriteEntry(new LogMessage(LogSeverity.Info, "Client", "client status changed."));
-
+                    discordNET.Client.SetGameAsync(status);
                     //_client.SetGameAsync(status);
                 }
                 if (input.StartsWith("setgch"))
@@ -629,17 +617,19 @@ namespace ModularBOT.Component
                         WriteEntry(new LogMessage(LogSeverity.Error, "Console", "Invalid ULONG."));
                         continue;
                     }
+                    WriteEntry(new LogMessage(LogSeverity.Error, "Console", "Set guild channel id."));
+
                 }
                 if (input.StartsWith("conmsg"))
                 {
                     input = input.Remove(0, 6).Trim();
-                    //SocketTextChannel Channel = _client.GetChannel(chID) as SocketTextChannel;
-                    //if (Channel == null)
-                    //{
-                    //    LogToConsole(new LogMessage(LogSeverity.Warning, "Console", "Invalid channel."));
-                    //    continue;
-                    //}
-                    //Channel.SendMessageAsync(input);
+                    SocketTextChannel Channel = discordNET.Client.GetChannel(chID) as SocketTextChannel;
+                    if (Channel == null)
+                    {
+                        WriteEntry(new LogMessage(LogSeverity.Error, "Console", "Invalid channel."));
+                        continue;
+                    }
+                    Channel.SendMessageAsync(input);
                 }
                 if (input.StartsWith("setvar"))
                 {
@@ -651,11 +641,11 @@ namespace ModularBOT.Component
                 }
 
             }
-           
+
         }
-        
+
         #region Console pixel art - Slight modification of https://stackoverflow.com/a/33715138/4655190
-        static int[] cColors = { 0x000000, 0x000080, 0x008000, 0x008080, 0x800000, 0x800080, 0x808000, 0xC0C0C0, 0x808080, 0x0000FF, 0x00FF00, 0x00FFFF, 0xFF0000, 0xFF00FF, 0xFFFF00, 0xFFFFFF };
+        int[] cColors = { 0x000000, 0x000080, 0x008000, 0x008080, 0x800000, 0x800080, 0x808000, 0xC0C0C0, 0x808080, 0x0000FF, 0x00FF00, 0x00FFFF, 0xFF0000, 0xFF00FF, 0xFFFF00, 0xFFFFFF };
 
         public void ConsoleWritePixel(System.Drawing.Color cValue)
         {
@@ -693,7 +683,7 @@ namespace ModularBOT.Component
 
         public void ConsoleWriteImage(System.Drawing.Bitmap source)
         {
-            
+
             int sMax = 39;
             decimal percent = Math.Min(decimal.Divide(sMax, source.Width), decimal.Divide(sMax, source.Height));
             System.Drawing.Size dSize = new System.Drawing.Size((int)(source.Width * percent), (int)(source.Height * percent));
