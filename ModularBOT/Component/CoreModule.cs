4d7cd604275e968df7a31d24c259d8bc040cc4be
TheKingEagle <rmsoftware1@gmail.com>
2019-04-06 13:44:16 -0400

    Added coreModule base

---
+++ ModularBOT/Component/CoreModule.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace ModularBOT.Component
+{
+    public class CoreModule:ModuleBase
+    {
+        DiscordShardedClient Client { get; set; }
+
+        CommandService Cmdsvr { get; set; }
+
+        ConsoleIO consoleIO { get; set; }
+
+        DiscordNET net { get; set; }
+        
+        public CoreModule(DiscordShardedClient client, CommandService cmdservice, ConsoleIO consoleIO, DiscordNET dnet)
+        {
+            Client = client;
+            Cmdsvr = cmdservice;
+            this.consoleIO = consoleIO;
+            net = dnet;
+            this.consoleIO.WriteEntry(new LogMessage(LogSeverity.Critical, "CoreMOD", "Constructor called! This debug message proved it."));
+
+        }
+
+        [Command("about"), Summary("Display information about the bot")]
+        public async Task ShowAbout()
+        {
+            EmbedBuilder builder = new EmbedBuilder
+            {
+                Title = "About"
+            };
+            builder.WithAuthor(Context.Client.CurrentUser);
+            builder.Color = Color.Blue;
+            builder.Description = "A Multi-purpose, multi-module bot designed for discord. Tailor it for your specific server, create your own modules and plug-ins. Includes a core module for custom text-based commands & EXEC functionality";
+            builder.AddField("Copyright", "Copyright © 2017-2019 RMSoftware Development");
+            builder.AddField("Version", Assembly.GetExecutingAssembly().GetName().Version.ToString(3));
+            builder.WithFooter("ModularBOT | created by TheKingEagle");
+            await Context.Channel.SendMessageAsync("", false, builder.Build());
+        }
+        [Command("addcmd"),Summary("Add a command to your bot")]
+        public async Task AddCmd(string cmdname, bool restricted, [Remainder]string action)
+        {
+            if (net.pmgr.GetAccessLevel(Context.User) < AccessLevels.CommandManager)
+            {
+                EmbedBuilder b = new EmbedBuilder();
+                b.WithTitle("Access Denied");
+                b.WithAuthor(Context.Client.CurrentUser);
+                b.WithDescription("You do not have permission to use this command. Requires `AccessLevel 1` or higher.");
+                b.WithColor(Color.Red);
+                b.WithFooter("ModularBOT • Core");
+                await Context.Channel.SendMessageAsync("", false, b.Build());
+            }
+            await net.ccmgr.AddCmd(Context.Message, cmdname, action, restricted);
+        }
+    }
+}
