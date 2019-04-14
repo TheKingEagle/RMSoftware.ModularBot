17c2d30e2c8c36109f902478d251a3ac4157666b
TheKingEagle <rmsoftware1@gmail.com>
2019-04-14 11:19:35 -0400

    version tick

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,9 +26,9 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.354.1543")]
-[assembly: AssemblyFileVersion("2.0.354.1543")]
+[assembly: AssemblyVersion("2.0.355.1544")]
+[assembly: AssemblyFileVersion("2.0.355.1544")]
 [assembly: ComVisible(false)]
 
 
-[assembly: AssemblyInformationalVersion("2.0.0-alpha-149")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha-150")]
