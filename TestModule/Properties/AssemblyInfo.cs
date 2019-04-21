5c0318522b5934f17e703e3b46a0495c091b0380
TheKingEagle <rmsoftware1@gmail.com>
2019-04-21 12:01:10 -0400

    Build 5; Beta-04

--- TestModule/Properties/AssemblyInfo.cs
+++ TestModule/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.3")]
-[assembly: AssemblyFileVersion("1.0.3.3")]
+[assembly: AssemblyVersion("1.0.5.5")]
+[assembly: AssemblyFileVersion("1.0.5.5")]
 //alpha/beta informational.
-[assembly: AssemblyInformationalVersion("1.0.1-beta-02")]
+[assembly: AssemblyInformationalVersion("1.0.1-beta-04")]
