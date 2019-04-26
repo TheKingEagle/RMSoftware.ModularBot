be38f84ff3ed8c15d0d93746b792b3ded6f0cc5d
TheKingEagle <rmsoftware1@gmail.com>
2019-04-26 14:30:44 -0400

    Build 1649; beta-11

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,9 +26,9 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.441.1630")]
-[assembly: AssemblyFileVersion("2.0.441.1630")]
+[assembly: AssemblyVersion("2.0.460.1649")]
+[assembly: AssemblyFileVersion("2.0.460.1649")]
 [assembly: ComVisible(false)]
 
 
-[assembly: AssemblyInformationalVersion("2.0.0-alpha-236")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta-11")]
