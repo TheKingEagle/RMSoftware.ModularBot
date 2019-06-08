b1bfbc38ba95d47c938d322a71a09e96ef3832fa
TheKingEagle <rmsoftware1@gmail.com>
2019-06-08 15:09:26 -0400

    Implementation changes
    PassiveModding/Discord.Addons.Interactive

--- Discord.Addons.Interactive/InteractiveBase.cs
+++ Discord.Addons.Interactive/InteractiveBase.cs
@@ -1,46 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Discord.Commands;
-using Discord.WebSocket;
-
-namespace Discord.Addons.Interactive
+﻿namespace Discord.Addons.Interactive
 {
-    public abstract class InteractiveBase : InteractiveBase<CommandContext>
+    using System;
+    using System.Threading.Tasks;
+
+    using Discord.Commands;
+    using Discord.WebSocket;
+
+    /// <summary>
+    /// The interactive base.
+    /// </summary>
+    public class InteractiveBase : InteractiveBase<CommandContext>
     {
     }
 
-    public abstract class InteractiveBase<T> : ModuleBase<T>
+    /// <summary>
+    /// The interactive base.
+    /// </summary>
+    /// <typeparam name="T">
+    /// </typeparam>
+    public class InteractiveBase<T> : ModuleBase<T>
         where T : CommandContext
     {
+        /// <summary>
+        /// Gets or sets the interactive service.
+        /// </summary>
         public InteractiveService Interactive { get; set; }
 
-        public Task<SocketMessage> NextMessageAsync(ICriterion<SocketMessage> criterion, TimeSpan? timeout = null, CancellationToken token = default(CancellationToken))
-            => Interactive.NextMessageAsync(Context, criterion, timeout, token);
-        public Task<SocketMessage> NextMessageAsync(bool fromSourceUser = true, bool inSourceChannel = true, TimeSpan? timeout = null, CancellationToken token = default(CancellationToken)) 
-            => Interactive.NextMessageAsync(Context, fromSourceUser, inSourceChannel, timeout, token);
+        public Task<SocketMessage> NextMessageAsync(ICriterion<SocketMessage> criterion, TimeSpan? timeout = null)
+            => Interactive.NextMessageAsync(Context, criterion, timeout);
+        public Task<SocketMessage> NextMessageAsync(bool fromSourceUser = true, bool inSourceChannel = true, TimeSpan? timeout = null)
+            => Interactive.NextMessageAsync(Context, fromSourceUser, inSourceChannel, timeout);
 
         public Task<IUserMessage> ReplyAndDeleteAsync(string content, bool isTTS = false, Embed embed = null, TimeSpan? timeout = null, RequestOptions options = null)
             => Interactive.ReplyAndDeleteAsync(Context, content, isTTS, embed, timeout, options);
 
-        public Task<IUserMessage> PagedReplyAsync(IEnumerable<object> pages, bool fromSourceUser = true)
-        {
-            var pager = new PaginatedMessage
-            {
-                Pages = (IEnumerable<PaginatedMessage.Page>)pages
-            };
-            return PagedReplyAsync(pager, fromSourceUser);
-        }
-        public Task<IUserMessage> PagedReplyAsync(PaginatedMessage pager, bool fromSourceUser = true)
+        public Task<IUserMessage> InlineReactionReplyAsync(ReactionCallbackData data, bool fromSourceUser = true)
+            => Interactive.SendMessageWithReactionCallbacksAsync(Context, data, fromSourceUser);
+
+        public Task<IUserMessage> PagedReplyAsync(PaginatedMessage pager, ReactionList Reactions, bool fromSourceUser = true)
         {
             var criterion = new Criteria<SocketReaction>();
             if (fromSourceUser)
                 criterion.AddCriterion(new EnsureReactionFromSourceUserCriterion());
-            return PagedReplyAsync(pager, criterion);
+            return PagedReplyAsync(pager, criterion, Reactions);
         }
-        public Task<IUserMessage> PagedReplyAsync(PaginatedMessage pager, ICriterion<SocketReaction> criterion)
-            => Interactive.SendPaginatedMessageAsync(Context, pager, criterion);
+        public Task<IUserMessage> PagedReplyAsync(PaginatedMessage pager, ICriterion<SocketReaction> criterion, ReactionList Reactions)
+            => Interactive.SendPaginatedMessageAsync(Context, pager, Reactions, criterion);
 
         public RuntimeResult Ok(string reason = null) => new OkResult(reason);
     }
