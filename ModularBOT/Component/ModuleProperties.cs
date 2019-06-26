7c17f8168b3fdf7a0435a1fa6a3cf901e9092236
TheKingEagle <rmsoftware1@gmail.com>
2019-06-26 09:00:22 -0400

    Per-guild module changes
    • AvailableGuild property type
    • Array validation
    • Code formatting.

--- ModularBOT/Component/ModuleProperties.cs
+++ ModularBOT/Component/ModuleProperties.cs
@@ -16,7 +16,7 @@ namespace ModularBOT.Component
     {
         public string ModuleName { get; set; }
         public string ServiceClass { get;  set; }
-        public ulong GuildAvailable { get;  set; }
+        public List<ulong> GuildsAvailable { get;  set; }
     }
 
     public class ModuleManager
@@ -30,7 +30,8 @@ namespace ModularBOT.Component
             //LOAD MODULES AND SERVICES.
             foreach (string item in Directory.EnumerateFiles("modules", "*.dll", SearchOption.TopDirectoryOnly))
             {
-                serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Info, "Modules", $"Adding commands from module library: {item}"), ConsoleColor.DarkGreen);
+                serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Info, 
+                    "Modules", $"Adding commands from module library: {item}"), ConsoleColor.DarkGreen);
                 try
                 {
                     Assembly asmb = Assembly.LoadFile(Path.GetFullPath(item));
@@ -44,14 +45,31 @@ namespace ModularBOT.Component
                             ModulePropertyItem propertyItem = JsonConvert.DeserializeObject<ModulePropertyItem>(json);
                             if(!string.IsNullOrWhiteSpace(propertyItem.ServiceClass))
                             {
-                                serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Verbose, "Modules", $"Service type specified! Injecting service: {propertyItem.ServiceClass} from {asmb.GetName().Name}"));
+                                serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Verbose, 
+                                    "Modules", $"Service type specified! Injecting service: {propertyItem.ServiceClass} from {asmb.GetName().Name}"));
                                 serviceCollection = serviceCollection.AddSingleton(asmb.GetType(propertyItem.ServiceClass));
                                 
                             }
 
                             if(string.IsNullOrWhiteSpace(propertyItem.ModuleName))
                             {
-                                serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical, "Modules", $"Type name is required! Unable to add module from {asmb.GetName().Name}"));
+                                serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical, 
+                                    "Modules", $"Type name is required! Unable to add module from {asmb.GetName().Name}"));
+                                continue;
+                            }
+                            if(propertyItem.GuildsAvailable != null)
+                            {
+                                if(propertyItem.GuildsAvailable.Count == 0)
+                                {
+                                    serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Info, 
+                                        "Modules", $"MPI didnt specify specific guild availability. Assuming ALL. Module: {asmb.GetName().Name}"));
+                                }
+                            }
+                            if (propertyItem.GuildsAvailable == null)
+                            {
+
+                                serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical,
+                                    "Modules", $"MPI's available guild list was null! Unable to load module: {asmb.GetName().Name}"));
                                 continue;
                             }
                             serviceProvider = serviceCollection.BuildServiceProvider();
@@ -61,14 +79,16 @@ namespace ModularBOT.Component
                     }
                     else
                     {
-                        serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical, "Modules", $"CRITICAL: No MPI found for the module. Cannot load module {asmb.GetName().Name}"));
+                        serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical, 
+                            "Modules", $"CRITICAL: No MPI found for the module. Cannot load module {asmb.GetName().Name}"));
                         continue;
                     }
                     
                 }
                 catch (Exception ex)
                 {
-                    serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical, "Modules", $"A Critical error occurred. could not load module {Path.GetFileName(item)}"));
+                    serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical, 
+                        "Modules", $"A Critical error occurred. could not load module {Path.GetFileName(item)}"));
                     serviceProvider.GetRequiredService<ConsoleIO>().WriteEntry(new LogMessage(LogSeverity.Critical, "Modules", ex.Message, ex));
 
                     using (FileStream fs = new FileStream("ERRORS.LOG", FileMode.Append))
