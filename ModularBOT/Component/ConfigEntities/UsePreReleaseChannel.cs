451a909487cbd5e6f1c6d1bb8be86f0bf5d4654c
TheKingEagle <rmsoftware1@gmail.com>
2019-07-27 20:41:28 -0400

    ConfigEntity: UsePreReleaseChannel

---
+++ ModularBOT/Component/ConfigEntities/UsePreReleaseChannel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using ModularBOT.Entity;
+using ModularBOT.Component;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ModularBOT.Component.ConfigEntities
+{
+    internal class CheckForUpdates : ConfigEntity
+    {
+        public CheckForUpdates()
+        {
+            ReadOnly = true;
+            ConfigIdentifier = "CheckForUpdates";
+        }
+
+        public override EmbedFieldBuilder ExecuteView(DiscordNET _DiscordNet, ICommandContext Context, bool inline)
+        {
+            EmbedFieldBuilder efb = new EmbedFieldBuilder()
+            {
+                Value = _DiscordNet.serviceProvider.GetRequiredService<Configuration>().CheckForUpdates.Value ? "`Yes`" : "`No`",
+                Name = ConfigIdentifier,
+                IsInline = inline
+            };
+            return efb;
+        }
+    }
+}
