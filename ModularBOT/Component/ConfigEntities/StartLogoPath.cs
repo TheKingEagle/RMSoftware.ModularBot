03c041df527d06d472ed1f3fdcb2e55a03d0728d
TheKingEagle <rmsoftware1@gmail.com>
2019-07-27 20:41:14 -0400

    ConfigEntity: StartLogoPath

---
+++ ModularBOT/Component/ConfigEntities/StartLogoPath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using ModularBOT.Entity;
+using ModularBOT.Component;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ModularBOT.Component.ConfigEntities
+{
+    internal class StartLogoPath : ConfigEntity
+    {
+        public StartLogoPath()
+        {
+            ReadOnly = true;
+            ConfigIdentifier = "StartLogoPath";
+        }
+
+        public override EmbedFieldBuilder ExecuteView(DiscordNET _DiscordNet, ICommandContext Context, bool inline)
+        {
+            EmbedFieldBuilder efb = new EmbedFieldBuilder()
+            {
+                Value = $"`{_DiscordNet.serviceProvider.GetRequiredService<Configuration>().LogoPath}`",
+                Name = ConfigIdentifier,
+                IsInline = inline
+            };
+            return efb;
+        }
+    }
+}
