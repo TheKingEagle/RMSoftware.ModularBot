478943bf1cd149547fc4ea4114cd8b5fd444ce10
TheKingEagle <rmsoftware1@gmail.com>
2019-08-17 17:07:39 -0400

    Add ServerMuteRole Config Entity

---
+++ TestModule/ConfigEntities/ServerMuteRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using ModularBOT.Component;
+using ModularBOT.Entity;
+namespace TestModule.ConfigEntities
+{
+    internal class ServerMuteRole : ConfigEntity
+    {
+        public ServerMuteRole()
+        {
+            ReadOnly = true;
+            ConfigIdentifier = "ServerMuteRole";
+        }
+        public override string ExecuteView(DiscordNET _DiscordNet, ICommandContext Context)
+        {
+            return base.ExecuteView(_DiscordNet, Context,TestModuleService.MLbindings.FirstOrDefault(x=>x.GuildID == Context.Guild.Id)?.MuteRoleID.ToString() ?? "Not Configured");
+        }
+    }
+}
