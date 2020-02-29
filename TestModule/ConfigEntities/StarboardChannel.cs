da66f194e80c06922b8471b1535feeb1c9d3a71d
TheKingEagle <rmsoftware1@gmail.com>
2020-02-28 23:28:02 -0500

    TMS: Added StarboardChannel

---
+++ TestModule/ConfigEntities/StarboardChannel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using ModularBOT.Component;
+using ModularBOT.Entity;
+namespace TestModule.ConfigEntities
+{
+    internal class StarboardChannel : ConfigEntity
+    {
+        public StarboardChannel()
+        {
+            ReadOnly = false;
+            ConfigIdentifier = "StarboardChannel";
+        }
+        public override string ExecuteView(DiscordNET _DiscordNet, ICommandContext Context)
+        {
+            //throw new NotImplementedException("Pending implementation");
+            //TODO: Implement StarboardChannel Config Entity;.SpyroLove
+            if(TestModuleService.SBBindings.TryGetValue(Context.Guild.Id,out StarboardBinding binding))
+            {
+                return base.ExecuteView(_DiscordNet, Context, binding.ChannelID.ToString());
+            }
+            return base.ExecuteView(_DiscordNet, Context, "Not Configured");
+        }
+
+        public override async Task ExecuteSet(DiscordShardedClient Client, DiscordNET _discordNET, ICommandContext Context, string value)
+        {
+            if(!ulong.TryParse(value, out ulong channelid))
+            {
+                await Context.Channel.SendMessageAsync("", false, 
+                    TestModuleService.GetEmbeddedMessage(Context, "Invalid ID", "Value must be a valid number.", Discord.Color.DarkRed));
+            }
+            await TestModuleService.BindStarboard(Context,channelid);
+            
+        }
+    }
+}
