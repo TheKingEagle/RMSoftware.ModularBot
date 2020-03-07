6ab5a2f4f3a83b16ffdbb111cf9898a16c860f26
TheKingEagle <rmsoftware1@gmail.com>
2020-03-07 18:52:54 -0500

    MB> CE-UsePreReleaseChannel: Add Setter

--- ModularBOT/Component/ConfigEntities/UsePreReleaseChannel.cs
+++ ModularBOT/Component/ConfigEntities/UsePreReleaseChannel.cs
@@ -16,7 +16,7 @@ namespace ModularBOT.Component.ConfigEntities
     {
         public UsePreReleaseChannel()
         {
-            ReadOnly = true;
+            ReadOnly = false;
             ConfigIdentifier = "UsePreReleaseChannel";
         }
 
@@ -24,17 +24,35 @@ namespace ModularBOT.Component.ConfigEntities
         {
             EmbedFieldBuilder efb = new EmbedFieldBuilder()
             {
-                Value = _DiscordNet.serviceProvider.GetRequiredService<Configuration>().UsePreReleaseChannel.Value ? "`Yes`" : "`No`",
+                Value = _DiscordNet.serviceProvider.GetRequiredService<Configuration>().UsePreReleaseChannel.Value ? "`True`" : "`False`",
                 Name = ConfigIdentifier,
                 IsInline = inline
             };
             return efb;
         }
 
-        public override string ExecuteView(DiscordNET _DiscordNet, ICommandContext Context)
+        public override string ExecuteView(DiscordNET _discordNET, ICommandContext Context)
         {
-            return base.ExecuteView(_DiscordNet, Context, 
-                _DiscordNet.serviceProvider.GetRequiredService<Configuration>().UsePreReleaseChannel.Value ? "Yes" : "No");
+            return base.ExecuteView(_discordNET, Context, 
+                _discordNET.serviceProvider.GetRequiredService<Configuration>().UsePreReleaseChannel.Value ? "True" : "False");
+        }
+
+        public override Task ExecuteSet(DiscordShardedClient Client, DiscordNET _discordNET, ICommandContext Context, string value)
+        {
+            if(_discordNET.PermissionManager.GetAccessLevel(Context.User) < AccessLevels.Administrator)
+            {
+                return Context.Channel.SendMessageAsync("", false, _discordNET.PermissionManager.GetAccessDeniedMessage(Context, AccessLevels.Administrator));
+            }
+            if(!bool.TryParse(value,out bool newval))
+            {
+                return Context.Channel.SendMessageAsync("", false, _discordNET.CustomCMDMgr.GetEmbeddedMessage(Context, 
+                    "Unexpected Value", "This configuration only accepts a boolean value `True` or `False`.", Color.DarkRed));
+            }
+            _discordNET.serviceProvider.GetRequiredService<Configuration>().UsePreReleaseChannel = newval;
+            _discordNET.serviceProvider.GetRequiredService<ConfigurationManager>().Save();
+
+            return Context.Channel.SendMessageAsync("", false, _discordNET.CustomCMDMgr.GetEmbeddedMessage(Context,
+                    "Configuration Updated", $"Program uses pre-release update channel: `{newval}`.\r\n\r\nThis change will not be effective until the program is restarted.", Color.Green));
         }
     }
 }
