ac104cc73df41ece8709c98622976c909bc7b260
TheKingEagle <rmsoftware1@gmail.com>
2020-03-07 18:52:37 -0500

    MB> CE-ShardCount: Add Setter

--- ModularBOT/Component/ConfigEntities/ShardCount.cs
+++ ModularBOT/Component/ConfigEntities/ShardCount.cs
@@ -16,7 +16,7 @@ namespace ModularBOT.Component.ConfigEntities
     {
         public ShardCount()
         {
-            ReadOnly = true;
+            ReadOnly = false;
             ConfigIdentifier = "ShardCount";
         }
 
@@ -33,6 +33,28 @@ namespace ModularBOT.Component.ConfigEntities
         public override string ExecuteView(DiscordNET _DiscordNet, ICommandContext Context)
         {
             return base.ExecuteView(_DiscordNet, Context,_DiscordNet.serviceProvider.GetRequiredService<Configuration>().ShardCount.ToString());
+        }
+
+        public override Task ExecuteSet(DiscordShardedClient Client, DiscordNET _discordNET, ICommandContext Context, string value)
+        {
+            if (_discordNET.PermissionManager.GetAccessLevel(Context.User) < AccessLevels.Administrator)
+            {
+                return Context.Channel.SendMessageAsync("", false, _discordNET.PermissionManager.GetAccessDeniedMessage(Context, AccessLevels.Administrator));
+            }
+            if (!int.TryParse(value, out int newval))
+            {
+                return Context.Channel.SendMessageAsync("", false, _discordNET.CustomCMDMgr.GetEmbeddedMessage(Context,
+                    "Unexpected Value", "This configuration only accepts a valid 32-bit integer greater than zero.", Color.DarkRed));
+            }
+            if (newval < 0)
+            {
+                return Context.Channel.SendMessageAsync("", false, _discordNET.CustomCMDMgr.GetEmbeddedMessage(Context,
+                    "Unexpected Value", "This configuration only accepts a valid 32-bit integer greater than zero.", Color.DarkRed));
+            }
+            _discordNET.serviceProvider.GetRequiredService<Configuration>().ShardCount = newval;
+            _discordNET.serviceProvider.GetRequiredService<ConfigurationManager>().Save();
+            return Context.Channel.SendMessageAsync("", false, _discordNET.CustomCMDMgr.GetEmbeddedMessage(Context,
+                    "Configuration Updated", $"Client will connect to Discord with `{newval}` shard(s).\r\n\r\nThis change will not be effective until the program is restarted.", Color.Green));
         }
     }
 }
