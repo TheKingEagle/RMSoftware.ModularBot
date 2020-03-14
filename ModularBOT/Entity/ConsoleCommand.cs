7864aa12b58a7bd3fd090a6080d6ac77363e4e4a
TheKingEagle <rmsoftware1@gmail.com>
2020-03-14 18:42:25 -0400

    Add new ConsoleCommand Entity.

---
+++ ModularBOT/Entity/ConsoleCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModularBOT.Entity;
+using ModularBOT.Component;
+
+namespace ModularBOT.Entity
+{
+    public class ConsoleCommand
+    {
+        public string CommandName { get; set; }
+
+        public virtual void ExecuteAsync(string consoleInput, ref bool ShutdownCalled, ref bool RestartRequested, ref bool InputCanceled, ref DiscordNET discordNET)
+        {
+            //override
+            return;
+        }
+    }
+}
