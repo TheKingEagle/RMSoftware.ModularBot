2c3e00eb7d85ae89242b37a392934b024ec4394f
TheKingEagle <rmsoftware1@gmail.com>
2020-03-15 12:20:00 -0400

    MBConsoleCommands: CLSCommand

---
+++ ModularBOT/Component/ConsoleCommands/CLSCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModularBOT.Entity;
+using Discord;
+using Discord.Net;
+using ModularBOT.Component;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+namespace ModularBOT.Component.ConsoleCommands
+{
+    public class CLSCommand : ConsoleCommand
+    {
+        public CLSCommand()
+        {
+            CommandName = "cls";
+        }
+        public override bool Execute(string consoleInput, ref bool ShutdownCalled, ref bool RestartRequested, ref bool InputCanceled, ref DiscordNET discordNET, ref ConsoleIO console)
+        {
+            console.LogEntries.Clear();//remove buffer.
+            console.ConsoleGUIReset(console.ConsoleForegroundColor, console.ConsoleBackgroundColor, console.ConsoleTitle);
+            SpinWait.SpinUntil(() => !ConsoleIO.ScreenBusy);
+            console.WriteEntry(new LogMessage(LogSeverity.Info, "Console", "Console cleared!"), null, true, false, true);
+            return true;
+            //return base.Execute(consoleInput, ref ShutdownCalled, ref RestartRequested, ref InputCanceled, ref discordNET);
+        }
+    }
+}
