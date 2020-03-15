11c087905f2c1beff2efac7923e14d1d42ac972b
TheKingEagle <rmsoftware1@gmail.com>
2020-03-15 12:24:21 -0400

    MBConsoleCommands: StatusCommand

---
+++ ModularBOT/Component/ConsoleCommands/StatusCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModularBOT.Entity;
+using Discord;
+using Discord.Net;
+using ModularBOT.Component;
+using Microsoft.Extensions.DependencyInjection;
+namespace ModularBOT.Component.ConsoleCommands
+{
+    public class StatusCommand : ConsoleCommand
+    {
+        public StatusCommand()
+        {
+            CommandName = "status";
+        }
+        public override bool Execute(string consoleInput, ref bool ShutdownCalled, ref bool RestartRequested, ref bool InputCanceled, ref DiscordNET discordNET, ref ConsoleIO console)
+        {
+            string status = consoleInput.Remove(0, CommandName.Length).Trim();
+            console.WriteEntry(new LogMessage(LogSeverity.Info, "Client", "client status changed."));
+            discordNET.Client.SetGameAsync(status);
+            return true;
+            //return base.Execute(consoleInput, ref ShutdownCalled, ref RestartRequested, ref InputCanceled, ref discordNET);
+        }
+    }
+}
