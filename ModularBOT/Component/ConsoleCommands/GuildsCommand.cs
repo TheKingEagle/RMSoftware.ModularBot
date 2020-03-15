68b0b4977e10dad6ddb36a5cccab0fffb2c80889
TheKingEagle <rmsoftware1@gmail.com>
2020-03-15 12:22:25 -0400

    MBConsoleCommands: GuildsCommand

---
+++ ModularBOT/Component/ConsoleCommands/GuildsCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModularBOT.Entity;
+using Discord;
+using Discord.Net;
+using ModularBOT.Component;
+using Microsoft.Extensions.DependencyInjection;
+using static ModularBOT.Component.ConsoleIO;
+
+namespace ModularBOT.Component.ConsoleCommands
+{
+    public class GuildsCommand : ConsoleCommand
+    {
+        public GuildsCommand()
+        {
+            CommandName = "guilds";
+        }
+        public override bool Execute(string consoleInput, ref bool ShutdownCalled, ref bool RestartRequested, ref bool InputCanceled, ref DiscordNET discordNET, ref ConsoleIO console)
+        {
+            string PRV_TITLE = console.ConsoleTitle;
+            List<LogEntry> v = new List<LogEntry>();
+            //---------------start modal---------------
+            console.ListGuilds(ref discordNET);
+            //----------------End modal----------------
+            console.ConsoleGUIReset(Program.configMGR.CurrentConfig.ConsoleForegroundColor,
+                Program.configMGR.CurrentConfig.ConsoleBackgroundColor, PRV_TITLE);
+            ScreenModal = false;
+            v.AddRange(console.LogEntries);
+            console.LogEntries.Clear();//clear buffer.
+                               //output previous logEntry.
+            foreach (var item in v)
+            {
+                console.WriteEntry(item.LogMessage, item.EntryColor);
+            }
+            return true;
+            //return base.Execute(consoleInput, ref ShutdownCalled, ref RestartRequested, ref InputCanceled, ref discordNET);
+        }
+    }
+}
