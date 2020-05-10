205d16d06995c3706d5a00f285eab6fd805ae89e
TheKingEagle <rmsoftware1@gmail.com>
2020-05-10 12:06:49 -0400

    ConsoleIO: Cleanup new users screen

--- ModularBOT/Component/ConsoleScreens/UsersScreen.cs
+++ ModularBOT/Component/ConsoleScreens/UsersScreen.cs
@@ -2,30 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using ModularBOT.Entity;
 using System.Reflection;
 using System.Threading;
 using Discord.WebSocket;
-using System.Windows;
-using Discord;
 
 namespace ModularBOT.Component.ConsoleScreens
 {
     public class UsersScreen : ConsoleScreen
     {
+        private short page = 1;
+        private readonly short max = 1;
+        private int index = 0;
+        private int selectionIndex = 0;
+        private int countOnPage = 0;
+        private int ppg = 0;
+        private bool errorfooter = false;
+        private readonly DiscordNET DNet;
         private List<SocketGuildUser> UserList = new List<SocketGuildUser>();
-        short page = 1;
-        short max = 1;
-        int index = 0;
-        int selectionIndex = 0;
-        int countOnPage = 0;
-        int ppg = 0;
-        bool errorfooter = false;
-        DiscordNET d;
+
         public UsersScreen(SocketGuild guild, DiscordNET discord, short startpage=1)
         {
-            d = discord;
+            DNet = discord;
             guild.DownloadUsersAsync();
             UserList = guild.Users.ToList().OrderByDescending(x => (int)(x.Hierarchy)).ToList();
             page = startpage;
@@ -64,19 +62,16 @@ namespace ModularBOT.Component.ConsoleScreens
             Console.CursorVisible = false;
             //TODO: Custom input handling: NOTE -- Base adds exit handler [E] key.
             if(ActivePrompt) { return false; }
+
             if (keyinfo.Key == ConsoleKey.P || keyinfo.Key == ConsoleKey.LeftArrow)
             {
-                
-                
                 if (page > 1)
                 {
                     page--;
                     index = (page * 22) - 22;//0 page 1 = 0; page 2 = 20; etc.
-                                             //continue;
                 }
                 selectionIndex = 0;
                 index = (page * 22) - 22;//0 page 1 = 0; page 2 = 20; etc.
-
 
                 if (ppg != page)
                 {
@@ -86,15 +81,15 @@ namespace ModularBOT.Component.ConsoleScreens
                     UpdateProgressBar();
                     ClearContents();
                     //RenderContents();
-                    countOnPage = PopulateList(d, page, max, ref index, selectionIndex, ref ppg, ref UserList);
+                    countOnPage = PopulateList(page, max, ref index, selectionIndex, ref ppg, ref UserList);
                 }
                     
                 ppg = page;
 
             }
+            
             if (keyinfo.Key == ConsoleKey.N || keyinfo.Key == ConsoleKey.RightArrow)
             {
-                
                 if (page < max)
                 {
 
@@ -109,8 +104,7 @@ namespace ModularBOT.Component.ConsoleScreens
                     UpdateProgressBar();
                     ClearContents();
                     //RenderContents();
-                    countOnPage = PopulateList(d, page, max, ref index, selectionIndex, ref ppg, ref UserList);
-
+                    countOnPage = PopulateList(page, max, ref index, selectionIndex, ref ppg, ref UserList);
                 }
                 ppg = page;
 
@@ -130,12 +124,11 @@ namespace ModularBOT.Component.ConsoleScreens
                     errorfooter = false;
                 }
                 index = (page * 22) - 22;//0 page 1 = 0; page 2 = 20; etc.
-                countOnPage = PopulateList(d, page, max, ref index, selectionIndex, ref ppg, ref UserList);
+                countOnPage = PopulateList(page, max, ref index, selectionIndex, ref ppg, ref UserList);
             }
+
             if (keyinfo.Key == ConsoleKey.DownArrow)
             {
-
-               
                 selectionIndex++;
                 if (selectionIndex > countOnPage - 1)
                 {
@@ -148,56 +141,53 @@ namespace ModularBOT.Component.ConsoleScreens
                     errorfooter = false;
                 }
                 index = (page * 22) - 22;//0 page 1 = 0; page 2 = 20; etc.
-                countOnPage = PopulateList(d, page, max, ref index, selectionIndex, ref ppg, ref UserList);
+                countOnPage = PopulateList(page, max, ref index, selectionIndex, ref ppg, ref UserList);
             }
 
             if (keyinfo.Key == ConsoleKey.Enter)
             {
-
                 index = (page * 22) - 22;//0 page 1 = 0; page 2 = 22; etc.
-                #region SubScreen
-                string username = SafeName(UserList, index + selectionIndex);
-                if ((d.PermissionManager.DefaultAdmin.EntityID == UserList[index+selectionIndex].Id))
+
+                string username = GetSafeName(UserList, index + selectionIndex);
+                if ((DNet.PermissionManager.DefaultAdmin.EntityID == UserList[index+selectionIndex].Id))
                 {
                     WriteFooter("[ERROR] You cannot modify this user.",ConsoleColor.DarkRed,ConsoleColor.White);
                     errorfooter = true;
                     return false;
                 }
-                //int left = 71-20;
-                //int top = 16 - 7;
-                //Console.CursorLeft = left;
-                //Console.CursorTop = top;
                 ConsoleColor PRVBG = Console.BackgroundColor;
                 ConsoleColor PRVFG = Console.ForegroundColor;
-                UpdateFooter(page, max, true);
-                int rr = ShowOptionSubScreen($"Editing: {username}", "Please select a new access level", "BlackListed", "Normal", "Command Manager", "Administrator");
+
+                #region ------------ [ACCESS LEVEL EDITOR] ------------
+
+                UpdateFooter(page, max, true);          //prompt footer
+                int rr = ShowOptionSubScreen($"Editing: {username}", "Please select a new access level",
+                    "BlackListed", "Normal", "Command Manager", "Administrator");
 
                 switch (rr)
                 {
                     case (1):
-                        d.PermissionManager.RegisterEntity(UserList[selectionIndex + index], AccessLevels.Blacklisted);
+                        DNet.PermissionManager.RegisterEntity(UserList[selectionIndex + index], AccessLevels.Blacklisted);
                         break;
                     case (2):
-                        if (d.PermissionManager.IsEntityRegistered(UserList[selectionIndex + index]))
+                        if (DNet.PermissionManager.IsEntityRegistered(UserList[selectionIndex + index]))
                         {
-                            d.PermissionManager.DeleteEntity(UserList[selectionIndex + index]);
+                            DNet.PermissionManager.DeleteEntity(UserList[selectionIndex + index]);
                         }
                         break;
                     case (3):
-                        d.PermissionManager.RegisterEntity(UserList[selectionIndex + index], AccessLevels.CommandManager);
+                        DNet.PermissionManager.RegisterEntity(UserList[selectionIndex + index], AccessLevels.CommandManager);
                         break;
                     case (4):
-                        d.PermissionManager.RegisterEntity(UserList[selectionIndex + index], AccessLevels.Administrator);
+                        DNet.PermissionManager.RegisterEntity(UserList[selectionIndex + index], AccessLevels.Administrator);
                         break;
                     default:
                         break;
                 }
 
+                UpdateFooter(page, max);                //restore footer
 
-
-                UpdateFooter(page, max);//update footer after all said and done.
                 #endregion
-
 
                 Console.ForegroundColor = PRVFG;
                 Console.BackgroundColor = PRVBG;
@@ -210,7 +200,7 @@ namespace ModularBOT.Component.ConsoleScreens
         {
             SpinWait.SpinUntil(() => !LayoutUpdating);
             SpinWait.SpinUntil(() => !ActivePrompt);
-            countOnPage = PopulateList(d, page, max, ref index, selectionIndex, ref ppg, ref UserList);
+            countOnPage = PopulateList(page, max, ref index, selectionIndex, ref ppg, ref UserList);
         }
 
         private void WriteFooter(string footer, ConsoleColor BackColor= ConsoleColor.Gray,ConsoleColor ForeColor = ConsoleColor.Black)
@@ -228,9 +218,10 @@ namespace ModularBOT.Component.ConsoleScreens
             LayoutUpdating = false;
         }
 
-        private int PopulateList(DiscordNET discord, short page, short max, ref int index, int selectionIndex, ref int ppg, ref List<SocketGuildUser> users)
+        private int PopulateList(short page, short max, ref int index, int selectionIndex, ref int ppg, ref List<SocketGuildUser> users)
         {
             int countOnPage;
+
             if (ppg != page)//is page changing?
             {
                 LayoutUpdating = true;
@@ -246,12 +237,15 @@ namespace ModularBOT.Component.ConsoleScreens
             }
 
             countOnPage = 0;
+
             if (ppg == page)
             {
                 Console.SetCursorPosition(0, ContentTop);
             }
+            
             WriteEntry($"\u2502\u2005\u2005\u2005 - {"Discord User".PadRight(39, '\u2005')} {"Entity ID".PadRight(22, '\u2005')} {"Access Level".PadLeft(16, '\u2005')} {"G. Admin".PadLeft(10, '\u2005')} {"G. Owner".PadLeft(10, '\u2005')}", ConsoleColor.Blue,false);
             WriteEntry($"\u2502\u2005\u2005\u2005 \u2500 {"".PadRight(39, '\u2500')} {"".PadLeft(22, '\u2500')} {"".PadLeft(16, '\u2500')} {"".PadLeft(10, '\u2500')} {"".PadLeft(10, '\u2500')}", ConsoleColor.Blue,false);
+            
             for (int i = index; i < 22 * page; i++)//22 results per page.
             {
                 if (i >= users.Count)
@@ -263,7 +257,6 @@ namespace ModularBOT.Component.ConsoleScreens
 
             }
 
-            
             Console.SetCursorPosition(0, 0);
             return countOnPage;
         }
@@ -271,11 +264,10 @@ namespace ModularBOT.Component.ConsoleScreens
         private void UpdateFooter(short page, short max, bool prompt=false)
         {
             LayoutUpdating = true;
+
             if (page > 1 && page < max)
             {
-
                 WriteFooter("[ESC] Exit \u2502 [N/RIGHT] Next Page \u2502 [P/LEFT] Previous Page \u2502 [UP/DOWN] Select \u2502 [ENTER] Properties...");
-
             }
             if (page == 1 && page < max)
             {
@@ -292,14 +284,13 @@ namespace ModularBOT.Component.ConsoleScreens
             if(prompt)
             {
                 WriteFooter("[Prompt] Please follow on-prompt instruction");
-
             }
+
             LayoutUpdating = false;
         }
 
         private void WriteListEntry(int selectionIndex, int countOnPage, List<SocketGuildUser> users, int i)
         {
-
             string name = users[i]?.Username ?? "[Pending...]";
 
             if (name.Length > 36)
@@ -311,21 +302,22 @@ namespace ModularBOT.Component.ConsoleScreens
             string p = $"{o}#{UserList.ElementAt(i).Discriminator}".PadRight(39, '\u2005');
             string admin = users[i].GuildPermissions.Administrator ? "Yes".PadLeft(10, '\u2005') : "No".PadLeft(10, '\u2005');
             string owner = users[i].Guild.OwnerId == users[i].Id ? "Yes".PadLeft(10, '\u2005') : "No".PadLeft(10, '\u2005');
-            string accesslevel = d.PermissionManager.GetAccessLevel(users.ElementAt(i)).ToString().PadRight(16,'\u2005');
+            string accesslevel = DNet.PermissionManager.GetAccessLevel(users.ElementAt(i)).ToString().PadRight(16,'\u2005');
             WriteEntry($"\u2502\u2005\u2005\u2005 - {p} [{users.ElementAt(i).Id.ToString().PadLeft(20, '0')}] {accesslevel} {admin} {owner} ", (countOnPage - 1) == selectionIndex, ConsoleColor.DarkGreen, false);
-
         }
 
-        private string SafeName(List<SocketGuildUser> users, int i)
+        private string GetSafeName(List<SocketGuildUser> users, int i)
         {
-            
             string userinput = users.ElementAt(i).Username ?? "[Pending...]";
             string o = Encoding.ASCII.GetString(Encoding.Convert(Encoding.Unicode, Encoding.GetEncoding(Encoding.ASCII.EncodingName, new EncoderReplacementFallback("?"), new DecoderExceptionFallback()), Encoding.Unicode.GetBytes(userinput))).Replace(' ', '\u2005').Replace("??", "?");
+            
             if (o.Length > 17)
             {
                 o = o.Remove(13) + "...";
             }
+
             string p = $"{o}";
+            
             return p;
         }
     }
