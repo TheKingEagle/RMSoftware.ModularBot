cd2f64343aae279bdeaf370ae986157cd391e770
TheKingEagle <rmsoftware1@gmail.com>
2020-05-26 11:41:11 -0400

    Build: v2.0.1475.2666 (spooky)

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1473.2664")]
-[assembly: AssemblyFileVersion("2.0.1473.2664")]
+[assembly: AssemblyVersion("2.0.1475.2666")]
+[assembly: AssemblyFileVersion("2.0.1475.2666")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyInformationalVersion("2.0.0-beta-150")]
