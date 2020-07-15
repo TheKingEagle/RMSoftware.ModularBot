f3c6b9df2d49eb705cf72ff32e7a9586b4bd8187
TheKingEagle <rmsoftware1@gmail.com>
2020-07-14 22:10:49 -0400

    BUILD: v2.0.1501.2692

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1496.2687")]
-[assembly: AssemblyFileVersion("2.0.1496.2687")]
+[assembly: AssemblyVersion("2.0.1501.2692")]
+[assembly: AssemblyFileVersion("2.0.1501.2692")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyInformationalVersion("2.0.0-beta-168")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta-170")]
