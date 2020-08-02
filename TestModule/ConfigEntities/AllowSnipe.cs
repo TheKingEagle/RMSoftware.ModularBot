41664944e8bf352b9de71483a59f896b0e68de43
TheKingEagle <rmsoftware1@gmail.com>
2020-08-02 01:04:30 -0400

    TMS_PROJECT_ADD: ConfigEntites -> Sniper

---
+++ TestModule/ConfigEntities/AllowSnipe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using ModularBOT.Component;
+using ModularBOT.Entity;
+namespace TestModule.ConfigEntities
+{
+    internal class AllowSnipe : ConfigEntity
+    {
+        public AllowSnipe()
+        {
+            ReadOnly = false;
+            ConfigIdentifier = "AllowSnipe";
+        }
+        public override string ExecuteView(DiscordNET _DiscordNet, ICommandContext Context)
+        {
+            var sniper = TestModuleService.SniperGuilds.FirstOrDefault(x => x.GuildID == Context.Guild.Id);
+            if (sniper == null) return base.ExecuteView(_DiscordNet, Context, "False");
+            else return base.ExecuteView(_DiscordNet, Context, "True");
+        }
+
+        public override async Task ExecuteSet(DiscordShardedClient Client, DiscordNET _discordNET, ICommandContext Context, string value)
+        {
+            if (_discordNET.PermissionManager.GetAccessLevel(Context.User) < AccessLevels.Administrator && !(Context.User as SocketGuildUser).GuildPermissions.Has(Discord.GuildPermission.ManageGuild))
+            {
+                await Context.Channel.SendMessageAsync("", false, TestModuleService.GetEmbeddedMessage(Context, "Insufficient Permission", "You need the ability to manage guild OR have `AccessLevels.Administrator`", Discord.Color.DarkRed));
+                return;
+            }
+
+            if (bool.TryParse(value, out bool result))
+            {
+                if (result) await TestModuleService.BindSniper(Context);
+                if (!result) await TestModuleService.UnBindSniper(Context);
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("", false, TestModuleService.GetEmbeddedMessage(Context, "Unexpected Value", "This item must be a BOOLEAN value. `True` or `False`.", Discord.Color.DarkRed));
+                return;
+            }
+            await Task.Delay(1);
+
+        }
+    }
+}
---
+++ TestModule/ConfigEntities/SniperQueueSize.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using ModularBOT.Component;
+using ModularBOT.Entity;
+namespace TestModule.ConfigEntities
+{
+    internal class SniperQueueSize : ConfigEntity
+    {
+        public SniperQueueSize()
+        {
+            ReadOnly = false;
+            ConfigIdentifier = "SniperQueueSize";
+        }
+        public override string ExecuteView(DiscordNET _DiscordNet, ICommandContext Context)
+        {
+
+            var sniper = TestModuleService.SniperGuilds.FirstOrDefault(x => x.GuildID == Context.Guild.Id);
+            if (sniper == null) return base.ExecuteView(_DiscordNet, Context, "Not Configured");
+            else return base.ExecuteView(_DiscordNet, Context, sniper.QueueSize.ToString());
+        }
+
+        public override async Task ExecuteSet(DiscordShardedClient Client, DiscordNET _discordNET, ICommandContext Context, string value)
+        {
+            if (_discordNET.PermissionManager.GetAccessLevel(Context.User) < AccessLevels.Administrator && !(Context.User as SocketGuildUser).GuildPermissions.Has(Discord.GuildPermission.ManageChannels))
+            {
+                await Context.Channel.SendMessageAsync("", false, TestModuleService.GetEmbeddedMessage(Context, "Insufficient Permission", "You need the ability to manage channels OR have `AccessLevels.Administrator`", Discord.Color.DarkRed));
+                return;
+            }
+            if(!int.TryParse(value, out int v))
+            {
+                await Context.Channel.SendMessageAsync("", false, TestModuleService.GetEmbeddedMessage(Context, "Unexpected Value", $"Please specify a value between `1` and `{int.MaxValue}`", Discord.Color.DarkRed));
+                return;
+            }
+            await TestModuleService.SetSniperQueueSize(Context, v);
+
+        }
+    }
+}
