a09f5b2c9735ac3b7f2cfa80dbac56fb90b6ab3f
TheKingEagle <rmsoftware1@gmail.com>
2020-08-13 19:34:15 -0400

    CHANGE: GuildsCommand to use new screen

--- ModularBOT/Component/ConsoleCommands/GuildsCommand.cs
+++ ModularBOT/Component/ConsoleCommands/GuildsCommand.cs
@@ -8,7 +8,9 @@ using Discord;
 using Discord.Net;
 using ModularBOT.Component;
 using Microsoft.Extensions.DependencyInjection;
+using ModularBOT.Component.ConsoleScreens;
 using static ModularBOT.Component.ConsoleIO;
+using System.Threading;
 
 namespace ModularBOT.Component.ConsoleCommands
 {
@@ -18,29 +20,38 @@ namespace ModularBOT.Component.ConsoleCommands
         {
             CommandName = "guilds";
         }
-
         public override bool Execute(string consoleInput, ref bool ShutdownCalled, ref bool RestartRequested, ref bool InputCanceled, ref DiscordNET discordNET, ref ConsoleIO console)
         {
+
             string PRV_TITLE = console.ConsoleTitle;
             List<LogEntry> v = new List<LogEntry>();
             ScreenModal = true;
             //---------------start modal---------------
-            console.ListGuilds(ref discordNET);
+            var NGScreen = new GuildsScreen(discordNET.Client.Guilds.ToList(),discordNET)
+            {
+                ActiveScreen = true
+            };
+            NGScreen.RenderScreen();
+            while (true)
+            {
+                if(NGScreen.ProcessInput(Console.ReadKey(true)))
+                {
+                    break;
+                }
+            }
+            NGScreen.ActiveScreen = false;
             //----------------End modal----------------
             console.ConsoleGUIReset(Program.configMGR.CurrentConfig.ConsoleForegroundColor,
                 Program.configMGR.CurrentConfig.ConsoleBackgroundColor, PRV_TITLE);
             ScreenModal = false;
             v.AddRange(console.LogEntries);
             console.LogEntries.Clear();//clear buffer.
-                               //output previous logEntry.
+                                       //output previous logEntry.
             foreach (var item in v)
             {
                 console.WriteEntry(item.LogMessage, item.EntryColor);
             }
             return true;
-            //return base.Execute(consoleInput, ref ShutdownCalled, ref RestartRequested, ref InputCanceled, ref discordNET);
         }
-
-
     }
 }
