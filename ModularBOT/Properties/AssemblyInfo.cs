726372227b401ac5a16a55ce110c4ac6fad0f703
TheKingEagle <rmsoftware1@gmail.com>
2020-08-30 08:13:26 -0400

    MB-BUILD: v2.0.1576.2767 (RELEASE BIN)

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1573.2764")]
-[assembly: AssemblyFileVersion("2.0.1573.2764")]
+[assembly: AssemblyVersion("2.0.1576.2767")]
+[assembly: AssemblyFileVersion("2.0.1576.2767")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyInformationalVersion("2.0.0-beta-234")]
