a593967585f26b6567c882e8e0f25719c18d9242
TheKingEagle <rmsoftware1@gmail.com>
2020-10-20 21:07:35 -0400

    MB-ADD: CSFunction class

---
+++ ModularBOT/Entity/CSFunction.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using ModularBOT.Component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModularBOT.Entity
+{
+    public class CSFunction
+    {
+        public string Name { get; protected set; }
+
+        public virtual async Task<bool> Evaluate(CoreScript engine, GuildObject gobj, string response, GuildCommand cmd, IDiscordClient client, IMessage message, EmbedBuilder errorEmbed, int LineInScript, string line, bool contextToDM, ulong ChannelTarget = 0, EmbedBuilder CSEmbed = null, bool StartCORE=false)
+        {
+            return await Task.FromResult(true);//derive... On error, returns false with ErrorEmbed set
+        }
+
+
+    }
+}
