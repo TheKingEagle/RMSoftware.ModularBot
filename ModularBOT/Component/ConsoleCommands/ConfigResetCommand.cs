e744080549bcaf6348861e78194c7056a05a5a61
TheKingEagle <rmsoftware1@gmail.com>
2020-10-25 20:21:00 -0400

    MB-ADD: Config.reset command

---
+++ ModularBOT/Component/ConsoleCommands/ConfigResetCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModularBOT.Entity;
+using Discord;
+using Discord.Net;
+using ModularBOT.Component;
+using Microsoft.Extensions.DependencyInjection;
+using static ModularBOT.Component.ConsoleIO;
+using ModularBOT.Component.ConsoleScreens;
+using System.Threading;
+
+namespace ModularBOT.Component.ConsoleCommands
+{
+    public class ConfigResetCommand : ConsoleCommand
+    {
+        public ConfigResetCommand()
+        {
+            CommandName = "config.reset";
+        }
+        public override bool Execute(string consoleInput, ref bool ShutdownCalled, ref bool RestartRequested, ref bool InputCanceled, ref DiscordNET discordNET, ref ConsoleIO console)
+        {
+
+            string PRV_TITLE = console.ConsoleTitle;
+            List<LogEntry> v = new List<LogEntry>();
+            ScreenModal = true;
+            //---------------start modal---------------
+            var NGScreen = new PromptScreen("Configuration Warning",
+                "   You are about to reset the configuration file. This will restart the application and run the initial setup wizard. " +
+                "\r\n   This will not remove installed modules, module configuration, nor any custom commands.")
+            {
+                ActiveScreen = true
+            };
+            ActiveScreen = NGScreen;
+            NGScreen.RenderScreen();
+            int res = NGScreen.Show("Reset Configuration?", "This action cannot be undone.", ConsoleColor.DarkRed, ConsoleColor.White);
+            
+            NGScreen.ActiveScreen = false; ConsoleIO.ActiveScreen = null;
+            //----------------End modal----------------
+            console.ConsoleGUIReset(Program.configMGR.CurrentConfig.ConsoleForegroundColor,
+                Program.configMGR.CurrentConfig.ConsoleBackgroundColor, PRV_TITLE);
+            ScreenModal = false;
+            v.AddRange(console.LogEntries);
+            console.LogEntries.Clear();//clear buffer.
+                                       //output previous logEntry.
+            foreach (var item in v)
+            {
+                console.WriteEntry(item.LogMessage, item.EntryColor);
+            }
+            if(res == 2)
+            {
+                console.WriteEntry(new LogMessage(LogSeverity.Critical, "Config", 
+                    "User initiated configuration reset. The application will now restart."), null, false, true, true);
+                Program.configMGR.Delete();
+                Thread.Sleep(2000);
+                RestartRequested = true;
+                ShutdownCalled = true;
+                Program.ImmediateTerm = true;
+            }
+            return true;
+        }
+    }
+}
