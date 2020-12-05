819fadc210dce97a0eaf350d50d89bf6a0889f6a
TheKingEagle <rmsoftware1@gmail.com>
2020-12-04 20:03:52 -0500

    MB-BUILD: v2.4.30.3238

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.28.3236")]
-[assembly: AssemblyFileVersion("2.4.28.3236")]
+[assembly: AssemblyVersion("2.4.30.3238")]
+[assembly: AssemblyFileVersion("2.4.30.3238")]
 [assembly: ComVisible(false)]
