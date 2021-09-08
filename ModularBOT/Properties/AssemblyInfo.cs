5439b39a4baf01b707e59cfaf50535676f67d4b1
rmsoft1 <rmsoftware1@gmail.com>
2021-09-08 18:15:08 -0400

    Update v2.5.12.3262

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.5.3255")]
-[assembly: AssemblyFileVersion("2.5.5.3255")]
+[assembly: AssemblyVersion("2.5.12.3262")]
+[assembly: AssemblyFileVersion("2.5.12.3262")]
 [assembly: ComVisible(false)]
