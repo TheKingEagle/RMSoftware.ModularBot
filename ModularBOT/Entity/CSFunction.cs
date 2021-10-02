799eed7612168664a45f8ffe721bcae1b75d3f03
rmsoft1 <rmsoftware1@gmail.com>
2021-10-02 05:42:23 -0400

    Add ScriptError function to CSFunction core (code reduction)

--- ModularBOT/Entity/CSFunction.cs
+++ ModularBOT/Entity/CSFunction.cs
@@ -17,6 +17,29 @@ namespace ModularBOT.Entity
             return await Task.FromResult(true);//derive... On error, returns false with ErrorEmbed set
         }
 
+        public bool ScriptError(string message, GuildCommand cmd, EmbedBuilder errorEmbed, int LineInScript, string line, EmbedFieldBuilder[] Additionalfields = null)
+        {
+            errorEmbed.WithDescription($"{message}\r\n```\r\n{line}\r\n```");
+            errorEmbed.AddField("Line", LineInScript, true);
+            errorEmbed.AddField("Execution Context", cmd?.Name ?? "No Context", true);
+            if(Additionalfields?.Length > 0)
+            {
+                errorEmbed.WithFields(Additionalfields);
+            }
+            return false;
+        }
 
+        public bool ScriptError(string message, string usage, GuildCommand cmd, EmbedBuilder errorEmbed, int LineInScript, string line, EmbedFieldBuilder[] Additionalfields = null)
+        {
+            errorEmbed.WithDescription($"{message}\r\n```\r\n{line}\r\n```");
+            errorEmbed.AddField("Usage", $"`{Name} {usage}`");
+            errorEmbed.AddField("Line", LineInScript, true);
+            errorEmbed.AddField("Execution Context", cmd?.Name ?? "No Context", true);
+            if (Additionalfields?.Length > 0)
+            {
+                errorEmbed.WithFields(Additionalfields);
+            }
+            return false;
+        }
     }
 }
