71b67c5937c0fc84dbe78691d9a69851fc6f618a
rmsoft1 <rmsoftware1@gmail.com>
2021-11-25 09:24:59 -0500

    Oops. I meant v2.7.30.3350

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.29.3349")]
-[assembly: AssemblyFileVersion("2.7.29.3349")]
+[assembly: AssemblyVersion("2.7.30.3350")]
+[assembly: AssemblyFileVersion("2.7.30.3350")]
 [assembly: ComVisible(false)]
