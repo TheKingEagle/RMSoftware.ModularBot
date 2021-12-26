4a9565623f26bf9e7ccc59584c2769342ed77619
rmsoft1 <rmsoftware1@gmail.com>
2021-12-26 16:57:50 -0500

    Purely for TMS update: v2.8.78.3731

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8.76.3729")]
-[assembly: AssemblyFileVersion("2.8.76.3729")]
+[assembly: AssemblyVersion("2.8.78.3731")]
+[assembly: AssemblyFileVersion("2.8.78.3731")]
 [assembly: ComVisible(false)]
