4a34a1a991cd96e1916d57bcbcc75188758ae2fe
rmsoft1 <rmsoftware1@gmail.com>
2022-01-03 12:13:51 -0500

    MB v2.9.0.3748

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8.80.3733")]
-[assembly: AssemblyFileVersion("2.8.80.3733")]
+[assembly: AssemblyVersion("2.9.0.3748")]
+[assembly: AssemblyFileVersion("2.9.0.3748")]
 [assembly: ComVisible(false)]
