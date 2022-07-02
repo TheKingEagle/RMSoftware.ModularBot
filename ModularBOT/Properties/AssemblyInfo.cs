bae487898f0d802e6279573799956b8f5d1da38b
rmsoft1 <rmsoftware1@gmail.com>
2022-07-01 22:16:07 -0400

    Rename ModuleProperties.cs to ModuleManager.cs; version bump 2.9.12.3760

--- ModularBOT/Properties/AssemblyInfo.cs
+++ ModularBOT/Properties/AssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.9.11.3759")]
-[assembly: AssemblyFileVersion("2.9.11.3759")]
+[assembly: AssemblyVersion("2.9.12.3760")]
+[assembly: AssemblyFileVersion("2.9.12.3760")]
 [assembly: ComVisible(false)]
